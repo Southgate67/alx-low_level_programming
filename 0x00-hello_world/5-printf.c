170a0a98d3e0c433331e5904fe2971efe73b5520
Southgate67 <southgate7626@gmail.com>
2023-08-11 06:38:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *this program prints a specified sentence
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
