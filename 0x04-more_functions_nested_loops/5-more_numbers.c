58572795b2fcf7c4f1f21fe52fde7dab2c48cc2c
Southgate67 <southgate7626@gmail.com>
2023-08-17 14:06:55 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet 10 times in lowercase
- * Description: print alpahebet 10 times in lowercase
+ *more_numbers - print 0 t 14 10 times
+ * Description: print given numbers 10 times
  * Return: void
  */
-void print_alphabet_x10(void)
+void more_numbers(void)
 {
 	int j;
 	int i;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = '0'; j < '14'; j++)
+		for (j = '0'; j < '15'; j++)
 			_putchar(j);
 				_putchar('\n');
 	}
